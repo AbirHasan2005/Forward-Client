57fb91306abf737cd83f19a541f254716b87e583
AbirHasan2005 <hackitabirhasan@gmail.com>
2021-04-13 18:22:13 +0600

    Update!

--- configs.py
+++ configs.py
@@ -1,6 +1,7 @@
 # (c) @AbirHasan2005
 
 import os
+import heroku3
 
 
 class Config(object):
@@ -15,3 +16,10 @@ class Config(object):
     FORWARD_TO_CHAT_ID = os.environ.get("FORWARD_TO_CHAT_ID", None)
     # Your User ID
     USER_ID = os.environ.get("USER_ID", None)
+    # Sleep Time while Kang
+    SLEEP_TIME = int(os.environ.get("SLEEP_TIME", 5))
+    # Heroku Management
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY")
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
+    HEROKU_APP = heroku3.from_key(HEROKU_API_KEY).apps()[HEROKU_APP_NAME] \
+        if HEROKU_API_KEY and HEROKU_APP_NAME else None
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # This is Telegram Messages Forwarder UserBot!
 # Use this at your own risk. I will not be responsible for any kind of issue while using this!
 
+import time
 import asyncio
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait, UserDeactivatedBan
@@ -13,14 +14,14 @@ User = Client(session_name=Config.STRING_SESSION, api_hash=Config.API_HASH, api_
 async def kanger(msg):
     await msg.edit(text="Forwarding Now ...")
     async for message in User.iter_history(chat_id=int(Config.FORWARD_FROM_CHAT_ID), reverse=True):
+        await asyncio.sleep(Config.SLEEP_TIME)
         try:
             await message.copy(int(Config.FORWARD_TO_CHAT_ID))
         except FloodWait as e:
             await User.send_message(chat_id="me", text=f"#FloodWait: Stopping Forwarder for `{e.x}s`!")
             await asyncio.sleep(e.x)
         except UserDeactivatedBan:
-            print(
-                "Congratulations!\nYour Account Banned Successfully!\nI already told you use a Fake Account. Hope you remember.")
+            print("Congratulations!\nYour Account Banned Successfully!\nI already told you use a Fake Account. Hope you remember.")
             break
         except Exception as err:
             await User.send_message(chat_id="me", text=f"#ERROR: `{err}`")
@@ -45,7 +46,20 @@ async def main(client, message):
         await message.edit(
             text="This UserBot can forward messages from any Chat to any other Chat also you can kang all messages from one Chat to another Chat.\n\n👨🏻‍💻 **Commands:**\n• `!start`\n• `!help`\n• `!kang`\n\n©️ **Developer:** @AbirHasan2005\n👥 **Support Group:** [【★ʟя★】](https://t.me/linux_repo)",
             parse_mode="Markdown", disable_web_page_preview=True)
-    elif message.text == "!kang" and (message.from_user.id == int(Config.USER_ID)):
+    elif message.text == "!restart" and (message.from_user.id == int(Config.USER_ID)):
+        if Config.HEROKU_APP is None:
+            await message.edit(
+                text="First Set `HEROKU_API_KEY` & `HEROKU_APP_NAME` to Restart Heroku Dynos!",
+                parse_mode="Markdown",
+                disable_web_page_preview=True
+            )
+        else:
+            await message.edit(
+                text="Restarting Heroku Dyno ..."
+            )
+            Config.HEROKU_APP.restart()
+            time.sleep(30)
+    elif message.text.startswith("!kang") and (message.from_user.id == int(Config.USER_ID)):
         editable = await message.edit(
             text=f"Trying to Get All Messages from `{str(Config.FORWARD_FROM_CHAT_ID)}` and Forwarding to `{str(Config.FORWARD_TO_CHAT_ID)}` ...",
             parse_mode="Markdown", disable_web_page_preview=True)
