22d0f89560e7650b35a4926444ac66a241e7022b
Abir Hasan <hackitabirhasan@gmail.com>
2022-09-03 12:48:05 +0600

    new feature (testing)

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 import sys
 import time
 import asyncio
-from pyrogram import Client, filters
+from pyrogram import Client, filters, enums
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from configs import Config
@@ -27,23 +27,26 @@ User = Client(
 async def main(client: Client, message: Message):
     if (-100 in Config.FORWARD_TO_CHAT_ID) or (-100 in Config.FORWARD_FROM_CHAT_ID):
         try:
-            await client.send_message(chat_id="me",
-                                      text=f"#VARS_MISSING: Please Set `FORWARD_FROM_CHAT_ID` or `FORWARD_TO_CHAT_ID` Config!")
+            await client.send_message(
+                chat_id="me",
+                text=f"#VARS_MISSING: Please Set `FORWARD_FROM_CHAT_ID` or `FORWARD_TO_CHAT_ID` Config!"
+            )
         except FloodWait as e:
             await asyncio.sleep(e.value)
         return
     if (message.text == "!start") and message.from_user.is_self:
         if not RUN["isRunning"]:
             RUN["isRunning"] = True
-        await message.edit(text=f"Hi, **{(await client.get_me()).first_name}**!\nThis is a Forwarder Userbot by @AbirHasan2005", parse_mode="Markdown",
-                           disable_web_page_preview=True)
+        await message.edit(
+            text=f"Hi, **{(await client.get_me()).first_name}**!\nThis is a Forwarder Userbot by @AbirHasan2005",
+            disable_web_page_preview=True)
     elif (message.text == "!stop") and message.from_user.is_self:
         RUN["isRunning"] = False
         return await message.edit("Userbot Stopped!\n\nSend `!start` to start userbot again.")
     elif (message.text == "!help") and message.from_user.is_self and RUN["isRunning"]:
         await message.edit(
             text=Config.HELP_TEXT,
-            parse_mode="Markdown", disable_web_page_preview=True)
+            disable_web_page_preview=True)
     elif message.text and (message.text.startswith("!add_forward_")) and message.from_user.is_self and RUN["isRunning"]:
         if len(message.text.split(" ", 1)) < 2:
             return await message.edit(f"{message.text} chat_id")
@@ -52,8 +55,26 @@ async def main(client: Client, message: Message):
                 Config.FORWARD_TO_CHAT_ID.append(int(x))
             elif x.isdigit() and message.text.startswith("!add_forward_from_chat"):
                 Config.FORWARD_FROM_CHAT_ID.append(int(x))
+            elif x.lower().startswith("all_joined_"):
+                chat_ids = []
+                if x.lower() == "all_joined_groups":
+                    await message.edit("Listing all joined groups ...")
+                    async for dialog in client.get_dialogs():
+                        chat = dialog.chat
+                        if chat and (chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]):
+                            chat_ids.append(chat.id)
+                if x.lower() == "all_joined_channels":
+                    await message.edit("Listing all joined channels ...")
+                    async for dialog in client.get_dialogs():
+                        chat = dialog.chat
+                        if chat and (chat.type == enums.ChatType.CHANNEL):
+                            chat_ids.append(chat.id)
+                if not chat_ids:
+                    return await message.edit("No Chats Found !!")
+                Config.FORWARD_FROM_CHAT_ID += chat_ids
             else:
                 pass
+        return await message.edit("Added Successfully!")
     elif message.text and (message.text.startswith("!remove_forward_")) and message.from_user.is_self and RUN["isRunning"]:
         if len(message.text.split(" ", 1)) < 2:
             return await message.edit(f"{message.text} chat_id")
@@ -64,11 +85,11 @@ async def main(client: Client, message: Message):
                 Config.FORWARD_FROM_CHAT_ID.remove(int(x))
             else:
                 pass
+        return await message.edit("Removed Successfully!")
     elif (message.text in ["!restart"]) and message.from_user.is_self:
         if Config.HEROKU_APP is None:
             await message.edit(
                 text="Restarting Userbot ...",
-                parse_mode="Markdown",
                 disable_web_page_preview=True
             )
             # https://stackoverflow.com/a/57032597/15215201
@@ -82,13 +103,14 @@ async def main(client: Client, message: Message):
     elif (message.text == "!kang") and message.from_user.is_self and RUN["isRunning"]:
         if len(Config.FORWARD_FROM_CHAT_ID) > 1:
             await message.edit(
-                text="Sorry Sir,\nWe can Kang only one Chat! But you put multiple Chat IDs in `FORWARD_FROM_CHAT_ID` Config!",
+                text="Sorry Sir,\n"
+                     "We can Kang only one Chat! But you put multiple Chat IDs in `FORWARD_FROM_CHAT_ID` Config!",
                 disable_web_page_preview=True
             )
             return
         await message.edit(
             text=f"Trying to Get All Messages from `{str(Config.FORWARD_FROM_CHAT_ID[0])}` and Forwarding to {' '.join(str(Config.FORWARD_TO_CHAT_ID))} ...",
-            parse_mode="Markdown", disable_web_page_preview=True)
+            disable_web_page_preview=True)
         await asyncio.sleep(5)
         try_kang = await Kanger(c=User, m=message)
         if try_kang == 400:
