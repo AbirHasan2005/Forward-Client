300987d76cd7c12fc58934466292b507c1d0ce7e
Abir Hasan <hackitabirhasan@gmail.com>
2022-11-12 19:46:29 +0600

    bug fix
    https://t.me/c/1575785190/131840

--- helpers/kanger.py
+++ helpers/kanger.py
@@ -23,7 +23,7 @@ async def Kanger(c: Client, m: Message):
         await asyncio.sleep(2)
         try:
             ForwardToChat = await c.get_chat_member(chat_id=Config.FORWARD_TO_CHAT_ID[i], user_id=(await c.get_me()).id)
-            if ForwardToChat.can_send_messages is False:
+            if not ForwardToChat.permissions.can_send_messages:
                 await m.edit(text=f"Sorry, you don't have permission to send messages in {ForwardToChat.title} !")
                 return 400
             await m.edit(text=f"Successfully Linked with `{ForwardToChat.title}` !")
